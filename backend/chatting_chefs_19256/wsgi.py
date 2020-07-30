1c2c599e44781e585e95c6f9f6077d7c2157a4ec
Crowdbotics <team@crowdbotics.com>
2020-07-30 00:37:36 +0000

    Update models

--- backend/chatting_chefs_19256/wsgi.py
+++ backend/chatting_chefs_19256/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatting_chefs_19256.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatting_chefs_19256.settings")
 
 application = get_wsgi_application()
